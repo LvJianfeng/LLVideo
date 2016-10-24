674502b0ca739c2714e86a1984c5d6c819b79cac
LvJianfeng <coderjianfeng@foxmail.com>
2016-10-24 14:58:33 +0800

    AVPlayer

--- LLVideoDemo/Class/RecordOne/RecordOneViewController.m
+++ LLVideoDemo/Class/RecordOne/RecordOneViewController.m
@@ -7,8 +7,10 @@
 //
 
 #import "RecordOneViewController.h"
+#import <AVKit/AVKit.h>
+#import <AVFoundation/AVFoundation.h>
 
-@interface RecordOneViewController ()
+@interface RecordOneViewController () <UIImagePickerControllerDelegate, UINavigationControllerDelegate>
 
 @end
 
@@ -22,4 +24,97 @@
     [super didReceiveMemoryWarning];
 }
 
+- (IBAction)record:(id)sender {
+    UIImagePickerController *ipc = [[UIImagePickerController alloc] init];
+    ipc.sourceType = UIImagePickerControllerSourceTypeCamera;//sourcetype有三种分别是camera，photoLibrary和photoAlbum
+    NSArray *availableMedia = [UIImagePickerController availableMediaTypesForSourceType:UIImagePickerControllerSourceTypeCamera];//Camera所支持的Media格式都有哪些,共有两个分别是@"public.image",@"public.movie"
+    ipc.mediaTypes = [NSArray arrayWithObject:availableMedia[1]];//设置媒体类型为public.movie
+    [self presentViewController:ipc animated:YES completion:nil];
+    ipc.videoMaximumDuration = 50;//30秒
+    ipc.delegate = self;//设置委托
+}
+
+/*
+ * 完成视频录制，并压缩后显示大小
+ */
+- (void)imagePickerController:(UIImagePickerController *)picker didFinishPickingMediaWithInfo:(NSDictionary *)info
+{
+    NSURL *sourceURL = [info objectForKey:UIImagePickerControllerMediaURL];
+    NSURL *newVideoUrl ; //一般.mp4
+    NSDateFormatter *formater = [[NSDateFormatter alloc] init];//用时间给文件全名，以免重复，在测试的时候其实可以判断文件是否存在若存在，则删除，重新生成文件即可
+    [formater setDateFormat:@"yyyy-MM-dd-HH:mm:ss"];
+    newVideoUrl = [NSURL fileURLWithPath:[NSHomeDirectory() stringByAppendingFormat:@"/Documents/output-%@.mp4", [formater stringFromDate:[NSDate date]]]] ;//这个是保存在app自己的沙盒路径里，后面可以选择是否在上传后删除掉。我建议删除掉，免得占空间。
+    [picker dismissViewControllerAnimated:YES completion:nil];
+    [self convertVideoQuailtyWithInputURL:sourceURL outputURL:newVideoUrl completeHandler:nil];
+}
+
+- (void) convertVideoQuailtyWithInputURL:(NSURL*)inputURL
+                               outputURL:(NSURL*)outputURL
+                         completeHandler:(void (^)(AVAssetExportSession*))handler
+{
+    AVURLAsset *avAsset = [AVURLAsset URLAssetWithURL:inputURL options:nil];
+    
+    AVAssetExportSession *exportSession = [[AVAssetExportSession alloc] initWithAsset:avAsset presetName:AVAssetExportPresetMediumQuality];
+    exportSession.outputURL = outputURL;
+    exportSession.outputFileType = AVFileTypeMPEG4;
+    exportSession.shouldOptimizeForNetworkUse= YES;
+    [exportSession exportAsynchronouslyWithCompletionHandler:^(void)
+     {
+         switch (exportSession.status) {
+             case AVAssetExportSessionStatusCancelled:
+                 NSLog(@"AVAssetExportSessionStatusCancelled");
+                 break;
+             case AVAssetExportSessionStatusUnknown:
+                 NSLog(@"AVAssetExportSessionStatusUnknown");
+                 break;
+             case AVAssetExportSessionStatusWaiting:
+                 NSLog(@"AVAssetExportSessionStatusWaiting");
+                 break;
+             case AVAssetExportSessionStatusExporting:
+                 NSLog(@"AVAssetExportSessionStatusExporting");
+                 break;
+             case AVAssetExportSessionStatusCompleted:
+                 NSLog(@"AVAssetExportSessionStatusCompleted");
+                 NSLog(@"%@", [NSString stringWithFormat:@"%.2f kb", [self getFileSize:[outputURL path]]]);
+                 
+                 UISaveVideoAtPathToSavedPhotosAlbum([outputURL path], self, nil, NULL);//这个是保存到手机相册
+                 
+                 [self alertUploadVideo:outputURL];
+                 break;
+             case AVAssetExportSessionStatusFailed:
+                 NSLog(@"AVAssetExportSessionStatusFailed");
+                 break;
+         }
+         
+     }];
+}
+
+-(void)alertUploadVideo:(NSURL*)URL{
+    CGFloat size = [self getFileSize:[URL path]];
+    CGFloat sizemb= size/1024;
+    
+    if(sizemb<5){
+        //小于5MB 直接上传
+        
+    }else{
+        //视频超过5MB 禁止上传
+    }
+    [[NSFileManager defaultManager] removeItemAtPath:[URL path] error:nil];
+    
+    
+}
+
+- (CGFloat) getFileSize:(NSString *)path
+{
+    NSFileManager *fileManager = [NSFileManager defaultManager];
+    float filesize = -1.0;
+    if ([fileManager fileExistsAtPath:path]) {
+        NSDictionary *fileDic = [fileManager attributesOfItemAtPath:path error:nil];//获取文件的属性
+        unsigned long long size = [[fileDic objectForKey:NSFileSize] longLongValue];
+        filesize = 1.0*size/1024;
+    }else{
+        NSLog(@"找不到文件");
+    }
+    return filesize;
+}
 @end
--- LLVideoDemo/Class/RecordTwo/RecordTwoViewController.m
+++ LLVideoDemo/Class/RecordTwo/RecordTwoViewController.m
@@ -7,20 +7,108 @@
 //
 
 #import "RecordTwoViewController.h"
+#import <AVFoundation/AVFoundation.h>
+#import <AssetsLibrary/AssetsLibrary.h>
 
-@interface RecordTwoViewController ()
+//屏幕宽
+#define ll_SCREEN_WIDTH ([[UIScreen mainScreen] bounds].size.width)
+//屏幕高
+#define ll_SCREEN_HEIGHT ([[UIScreen mainScreen] bounds].size.height)
 
+
+@interface RecordTwoViewController () <AVCaptureFileOutputRecordingDelegate>
+@property (nonatomic) dispatch_queue_t sessionQueue;
+/**
+ *  AVCaptureSession对象来执行输入设备和输出设备之间的数据传递
+ */
+@property (nonatomic, strong) AVCaptureSession* session;
+/**
+ *  视频输入设备
+ */
+@property (nonatomic, strong) AVCaptureDeviceInput* videoInput;
+/**
+ *  声音输入
+ */
+@property (nonatomic, strong) AVCaptureDeviceInput* audioInput;
+/**
+ *  视频输出流
+ */
+@property(nonatomic,strong)AVCaptureMovieFileOutput *movieFileOutput;
+/**
+ *  预览图层
+ */
+@property (nonatomic, strong) AVCaptureVideoPreviewLayer* previewLayer;
+/**
+ *  记录录制时间
+ */
+@property (nonatomic, strong) NSTimer* timer;
+@property (weak, nonatomic) IBOutlet UIView *capView;
 @end
 
 @implementation RecordTwoViewController
 
 - (void)viewDidLoad {
     [super viewDidLoad];
+    
+    [self initAVCaptureSession];
 }
 
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
 }
 
+- (void)initAVCaptureSession{
+    self.session = [[AVCaptureSession alloc] init];
+//    self.session.sessionPreset = AVAssetExportPreset640x480;
+    
+    NSError *error;
+    AVCaptureDevice *device = [AVCaptureDevice defaultDeviceWithMediaType:AVMediaTypeVideo];
+    [device lockForConfiguration:nil];
+    
+    [device unlockForConfiguration];
+    
+    self.videoInput = [[AVCaptureDeviceInput alloc] initWithDevice:device error:&error];
+    self.audioInput = [[AVCaptureDeviceInput alloc] initWithDevice:[AVCaptureDevice defaultDeviceWithMediaType:AVMediaTypeAudio] error:nil];
+    
+    if (error) {
+        NSLog(@"viewDidLoad->error->%@",error);
+    }
+    
+    self.movieFileOutput = [[AVCaptureMovieFileOutput alloc] init];
+    
+    if ([self.session canAddInput:self.videoInput]) {
+        [self.session addInput:self.videoInput];
+    }
+    
+    if ([self.session canAddInput:self.audioInput]) {
+        [self.session addInput:self.audioInput];
+    }
+    
+    if ([self.session canAddOutput:self.movieFileOutput]) {
+        [self.session addOutput:self.movieFileOutput];
+    }
+    
+    self.previewLayer = [[AVCaptureVideoPreviewLayer alloc] initWithSession:self.session];
+    [self.previewLayer setVideoGravity:AVLayerVideoGravityResizeAspect];
+    
+    self.previewLayer.frame = CGRectMake(0, 0, ll_SCREEN_WIDTH, 350);
+    self.capView.layer.masksToBounds = YES;
+    [self.capView.layer insertSublayer:self.previewLayer atIndex:0];
+    
+    [self.session startRunning];
+}
 
+
+
+/*
+ * 录制
+ */
+- (IBAction)record:(id)sender {
+    AVCaptureConnection *capConnection = [self.movieFileOutput connectionWithMediaType:AVMediaTypeVideo];
+    AVCaptureVideoOrientation capVideoOri = AVCaptureVideoOrientationPortrait;
+    [capConnection setVideoOrientation:capVideoOri];
+    [capConnection setVideoScaleAndCropFactor:1.0];
+    
+    
+}
 @end
